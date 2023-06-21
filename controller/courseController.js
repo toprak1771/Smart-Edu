da8a46357b1cd028d319448275706b99711c4d3b
Toprak Tüzün <topraktuzun17@hotmail.com>
2023-06-21 15:27:45 +0300

    teacher-course realtional

--- controller/courseController.js
+++ controller/courseController.js
@@ -3,7 +3,10 @@ const Category = require('../models/Category');
 
 exports.addCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+      ...req.body,
+      user:req.session.userID,
+    });
     res.status(201).redirect('/courses');
   } catch (error) {
     console.log("error:",error);
@@ -42,7 +45,7 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const course = await Course.findOne({ slug: req.params.slug });
+    const course = await Course.findOne({ slug: req.params.slug }).populate('user');
     res.status(200).render('course', {
       status: 'success',
       course,
