cd11d0444301968f117290ce5650ad620d5adc2c
Toprak Tüzün <topraktuzun17@hotmail.com>
2023-06-22 16:31:36 +0300

    complete relational structure

--- controller/courseController.js
+++ controller/courseController.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.addCourse = async (req, res) => {
   try {
@@ -51,6 +52,36 @@ exports.getCourse = async (req, res) => {
       course,
       page_name: 'course',
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.enrollCourse = async (req, res) => {
+  try {
+    const user = await User.findOne({_id:req.session.userID});
+    await user.courses.push({_id:req.body.courseID});
+    await user.save();
+
+    res.status(201).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.releaseCourse = async (req, res) => {
+  try {
+    const user = await User.findOne({_id:req.session.userID});
+    await user.courses.pull({_id:req.body.courseID});
+    await user.save()
+
+    res.status(201).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -6,4 +6,6 @@ const roleMiddleware = require('../middlewares/roleMiddleware');
 router.route('/add').post(roleMiddleware(["teacher","admin"]),courseController.addCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/enroll').post(courseController.enrollCourse);
+router.route('/release').post(courseController.releaseCourse);
 module.exports = router;
